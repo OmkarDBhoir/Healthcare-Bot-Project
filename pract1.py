753e0a68895f6c6c17025ac7a55cf121f5c48ef2
Victor <Omkar123b@gmail.com>
2022-03-28 22:15:50 +0530

    Web Url Connection

--- pract1.py
+++ pract1.py
@@ -3,9 +3,17 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/Home')
 def run_template():
     return render_template('index.html')
 
+@app.route('/Hospitals') 
+def Hospitals():
+    return render_template('Hospitals.html')
+
+@app.route('/Query')
+def Query():
+    return render_template('Query.html')
 
 
 if __name__ == '__main__':
